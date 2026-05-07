b82ea1bbc5bf06cc4161389696d45235851fef72
Sidhartha Maji <sidharthamaji04@gmail.com>
2026-05-07 16:13:13 +0530

    removes coin class
    * now chance only takes probability

--- test/com/bootcamp/p2/ChanceTest.java
+++ test/com/bootcamp/p2/ChanceTest.java
@@ -7,15 +7,33 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 public class ChanceTest {
     @Test
     void chanceOfSameTotalPossibilitiesAndFavorableShouldBeEqual() {
-        Chance c1 = new Chance(2, 1);
-        Chance c2 = new Chance(2, 1);
+        Chance c1 = new Chance(0.5);
+        Chance c2 = new Chance(0.5);
         assertEquals(c1, c2);
     }
 
     @Test
-    void valueOfChanceShouldBeFavorablePosibilitiesDividedByTotalPossibilities() {
-        Chance chance = new Chance(2, 1);
-        assertEquals(0.5, chance.value());
+    void chanceOfGettingTailsShouldBePoint5() {
+        Chance chanceOfGettingTails = new Chance(0.5);
+        assertEquals(0.5, chanceOfGettingTails.value());
+    }
+
+    @Test
+    void chanceOfNotGettingTailsShouldBePoint5() {
+        Chance chance = new Chance(0.5);
+        Chance chanceOfNotGettingTails = chance.not();
+        assertEquals(0.5, chanceOfNotGettingTails.value());
+    }
+
+    @Test
+    void chanceOfGettingTailsWhenTwoCoinsAreFlippedShouldBePoint75() {
+        Chance chanceOfGettingTails = new Chance(0.75);
+        assertEquals(0.75, chanceOfGettingTails.value());
+    }
+    @Test
+    void chanceOfGetting3INADice() {
+        Chance chanceOfGettingThree = new Chance(0.1667);
+        assertEquals(0.1667, chanceOfGettingThree.value());
     }
 
 }
