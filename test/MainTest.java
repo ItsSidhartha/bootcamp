ff02b1a92ae0bfb8a22424724dfc45efc5def3cd
Sidhartha Maji <sidharthamaji04@gmail.com>
2026-05-07 10:49:24 +0530

    sets up simple test and mockito

---
+++ test/MainTest.java
@@ -0,0 +1,13 @@
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
+
+class MainTest {
+    @Test
+    void name() {
+        assertEquals(1,1);
+
+    }
+}
