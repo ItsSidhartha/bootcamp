251e8be8d40f0ef35b64449a6b7aff5018c8f09a
Sidhartha Maji <sidharthamaji04@gmail.com>
2026-05-09 16:55:46 +0530

    adds color specific limitation in bag

--- src/com/tw/bootcamp/p5/Bag.java
+++ src/com/tw/bootcamp/p5/Bag.java
@@ -5,16 +5,39 @@ import java.util.HashSet;
 
 public class Bag {
     private final int capacity = 12;
+    private final int greenBallLimit;
     HashMap<BallColors, HashSet<Ball>> balls = new HashMap<>();
     private int occupied;
+
+    public Bag(int greenBallLimit) {
+        this.greenBallLimit = greenBallLimit;
+    }
 
     public boolean addBall(Ball ball) throws NotEnoughSpaceInBagException {
         if (occupied == capacity) throw new NotEnoughSpaceInBagException();
         balls.putIfAbsent(ball.getColor(), new HashSet<>());
-        boolean isAdded = balls.get(ball.getColor()).add(ball);
-        if (!isAdded) return false;
 
-        occupied++;
-        return true;
+        if (canAdd(ball)) {
+            boolean isAdded = balls.get(ball.getColor()).add(ball);
+
+            if (!isAdded) return false;
+            occupied++;
+            return true;
+        }
+        return false;
+    }
+
+    private boolean canAdd(Ball ball) {
+        return switch (ball.getColor()) {
+            case GREEN -> greenBallLimit > getSize(ball.getColor());
+            case RED -> getSize(BallColors.GREEN) * 2 > getSize(ball.getColor());
+            case YELLOW -> occupied * 0.4 > getSize(ball.getColor());
+            case BLUE -> true;
+        };
+    }
+
+    private int getSize(BallColors color) {
+        if (!balls.containsKey(color)) return 0;
+        return balls.get(color).size();
     }
 }
--- test/com/tw/bootcamp/p5/BagTest.java
+++ test/com/tw/bootcamp/p5/BagTest.java
@@ -2,23 +2,59 @@ package com.tw.bootcamp.p5;
 
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.assertThrows;
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class BagTest {
     @Test
     void ShouldBeAbleToAddBallAtATime() throws NotEnoughSpaceInBagException {
-        Bag bag = new Bag();
-        assertTrue(bag.addBall(new Ball(BallColors.RED)));
+        Bag bag = new Bag(3);
+        assertTrue(bag.addBall(new Ball(BallColors.GREEN)));
     }
 
     @Test
     void addShouldThrowExceptionIfBagIsFull() throws NotEnoughSpaceInBagException {
-        Bag bag = new Bag();
+        Bag bag = new Bag(3);
         for (int i = 0; i < 12; i++) {
             bag.addBall(new Ball(BallColors.BLUE));
         }
 
         assertThrows(NotEnoughSpaceInBagException.class, () -> bag.addBall(new Ball(BallColors.YELLOW)));
+    }
+
+    @Test
+    void shouldBeAbleToLimitCountOfGreenColorBall() throws NotEnoughSpaceInBagException {
+        Bag bag = new Bag(3);
+        boolean b1 = bag.addBall(new Ball(BallColors.GREEN));
+        boolean b2 = bag.addBall(new Ball(BallColors.GREEN));
+        boolean b3 = bag.addBall(new Ball(BallColors.GREEN));
+        assertTrue(b3);
+        boolean b4 = bag.addBall(new Ball(BallColors.GREEN));
+        assertFalse(b4);
+    }
+
+    @Test
+    void shouldBeAbleToLimitCountOfRedColorBall() throws NotEnoughSpaceInBagException {
+        Bag bag = new Bag(3);
+        boolean b1 = bag.addBall(new Ball(BallColors.GREEN));
+        boolean b2 = bag.addBall(new Ball(BallColors.RED));
+        boolean b3 = bag.addBall(new Ball(BallColors.RED));
+        assertTrue(b3);
+        boolean b4 = bag.addBall(new Ball(BallColors.RED));
+        assertFalse(b4);
+    }
+
+    @Test
+    void shouldBeAbleToLimitCountOfYellowColorBall() throws NotEnoughSpaceInBagException {
+        Bag bag = new Bag(3);
+        boolean b1 = bag.addBall(new Ball(BallColors.GREEN));
+        boolean b2 = bag.addBall(new Ball(BallColors.GREEN));
+        boolean b3 = bag.addBall(new Ball(BallColors.RED));
+        boolean b4 = bag.addBall(new Ball(BallColors.RED));
+        boolean b5 = bag.addBall(new Ball(BallColors.YELLOW));
+        assertTrue(b5);
+        boolean b6 = bag.addBall(new Ball(BallColors.YELLOW));
+        boolean b7 = bag.addBall(new Ball(BallColors.YELLOW));
+        boolean b8 = bag.addBall(new Ball(BallColors.YELLOW));
+        assertFalse(b8);
     }
 }
